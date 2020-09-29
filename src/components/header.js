be81f9e664479180d8098ec7bc181c6c2916120a
Michaela Wagner <mickywagner@gmail.com>
2020-09-29 12:47:38 -0600

    Change header styles

--- src/components/header.js
+++ src/components/header.js
@@ -5,28 +5,52 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
+      background:  `rgb(41, 41, 41)`,
+      marginBottom: `1.2rem`,
+      display: `flex`,
     }}
   >
     <div
       style={{
         margin: `0 auto`,
         maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        padding: `1rem 1rem`,
+        
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h2 style={{ 
+        margin: 0,
+        fontSize: `1rem`
+      }}>
         <Link
           to="/"
           style={{
             color: `white`,
-            textDecoration: `none`,
+            padding: `0 15px`
           }}
         >
-          {siteTitle}
+          Home
         </Link>
-      </h1>
+        <Link
+          to="/about/"
+          style={{
+            color: `white`,
+            padding: `0 15px`
+          }}
+        >
+          About
+        </Link>
+        <Link
+          to="/portfolio/"
+          style={{
+            color: `white`,
+            padding: `0 15px`
+          }}
+        >
+          Portfolio
+        </Link>
+        
+      </h2>
     </div>
   </header>
 )
